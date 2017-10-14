be0ce9fd6219669f66094bf33b34f6fc42c327b6
Loyc59 <ldoignie@gmail.com>
2017-10-14 08:31:12 +0200

    Finalisation sponsor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  sponsors1 = [
+    'nordnet.jpg',
+    'youtube.png',
+    'fretron.jpg'
+  ];
+  sponsors2 = [
+    'logo.jpg',
+    'samsung.jpg'
+  ];
+  index1: number = 0;
+  index2: number = 0;
+  
+  constructor() {
+    const timer = Observable.interval(2000);
+    timer.subscribe(() => {
+      this.index1 = this.index1 === this.sponsors1.length-1 ? 0 : this.index1 + 1;
+      this.index2 = this.index2 === this.sponsors2.length-1 ? 0 : this.index2 + 1;
+    });
+  }
 }
