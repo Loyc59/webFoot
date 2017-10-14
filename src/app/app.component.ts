06a33f9d0e62bbfb3435223287286ca4e41fafc0
NicoTh59 <thouvenin.nicolas@gmail.com>
2017-10-14 19:08:20 +0200

    Changement css classements + ajout Images logos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,44 @@ import { Observable } from 'rxjs/Rx';
 })
 export class AppComponent {
   sponsors1: Array<string> = [
-    'nordnet.jpg',
-    'youtube.png',
-    'fretron.jpg'
+    'ad.jpg',
+    'apadeloo.jpg',
+    'art_et_eau.jpg',
+    'bubblebump.jpg',
+    'clotures_melantois.jpg',
+    'credit_mutuel.jpg',
+    'decathlon.jpg',
+    'delices_pevele.jpg',
+    'domaine_funquerel.jpg',
+    'ecopevele.jpg',
+    'espe.jpg',
+    'ferme_acquette.jpg',
+    'funbridge.jpg',
+    'gojantes.jpg',
+    'guy_brunet.jpg',
+    'idimmobilier.jpg',
+    'jadeo.jpg',
+    'lacaleche.jpg'
   ];
   sponsors2: Array<string> = [
-    'logo.jpg',
-    'samsung.jpg'
+    'logo_losc.jpg',
+    'lou_coiffure.jpg',
+    'matylight.jpg',
+    'max_garage.jpg',
+    'mdweb.jpg',
+    'metropole_couverture.jpg',
+    'olympiades.jpg',
+    'patapata.jpg',
+    'perfect_deco.jpg',
+    'piraino.jpg',
+    'proprete.jpg',
+    'sd.jpg',
+    'sligec.jpg',
+    'terenvi.jpg',
+    'texaco.jpg',
+    'vaillant.jpg',
+    'xxl.jpg',
+    'zcars.jpg'
   ];
   indexSponsor1: number = 0;
   indexSponsor2: number = 0;
@@ -26,14 +57,14 @@ export class AppComponent {
     this.router.navigate([this.router.config[this.indexRouting].path]);
 
     //Switch sponsor images
-    const sponsorTimer = Observable.interval(2000);
+    const sponsorTimer = Observable.interval(3000);
     sponsorTimer.subscribe(() => {
       this.indexSponsor1 = this.indexSponsor1 === this.sponsors1.length-1 ? 0 : this.indexSponsor1 + 1;
       this.indexSponsor2 = this.indexSponsor2 === this.sponsors2.length-1 ? 0 : this.indexSponsor2 + 1;
     });
 
     //Switch ranking via routing
-    const rankingTimer = Observable.interval(12000);
+    const rankingTimer = Observable.interval(15000);
     rankingTimer.subscribe(() => {
       this.indexRouting = this.indexRouting === this.router.config.length-1 ? 0 : this.indexRouting + 1;
       this.router.navigate([this.router.config[this.indexRouting].path]);
